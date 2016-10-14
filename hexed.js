54250211be8494e589e58127f17376c5350c3998
Joseph Noel <jtnoel96@gmail.com>
2016-10-14 11:55:57 -0400

    Added jquery UI "Spinners" for difficulty and turns

--- hexed.js
+++ hexed.js
@@ -15,6 +15,41 @@ $(document).ready(function() {
 	   		}
 	   		// Add Title
 	   		this.append("<h1>Hexed</h1>");
+	   		// Add sliders
+	   		this.append("<div id='spinners'>\
+	   						<input type='text' id='difficulty' value='0'/>\
+	   						<input type='text' id='turns' value='0'/>\
+	   					</div>");
+	   		$("#difficulty").spinner({
+	   			min: 0,
+	   			max: 10,
+	   			change: function() { // limit greater/smaller values
+			        var min = $(this).spinner('option', 'min');
+			        var max = $(this).spinner('option', 'max');
+
+			        if (isNaN($(this).val())) {
+			            $(this).spinner("value", min);
+			        } else if ($(this).val() > max) {
+			            $(this).spinner("value", max);
+			        } else if ($(this).val() < min) {
+			            $(this).spinner("value", min);
+			        }
+				}
+	   		}).val(difficulty);
+	   		$("#turns").spinner({
+	   			min: 0,
+	   			change: function() { // limit greater/smaller values
+			        var min = $(this).spinner('option', 'min');
+			        var max = $(this).spinner('option', 'max');
+
+			        if (isNaN($(this).val())) {
+			            $(this).spinner("value", min);
+			        } else if ($(this).val() < min) {
+			            $(this).spinner("value", min);
+			        }
+				}
+	   		}).val(turns);
+
 	 		// Return self for chaining
 	        return this;
 	    };
