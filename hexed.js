84aa2f9c0a0fc787f2bad7891ccd7878af6f7eb2
Joseph Noel <jtnoel96@gmail.com>
2016-10-18 15:48:11 -0400

    Added scoring, proper button appearance, fixed new game

--- hexed.js
+++ hexed.js
@@ -3,9 +3,15 @@ $(document).ready(function() {
 	    $.fn.hexed = function( settings ) {
 	    	// Clear beggining of every game
 	    	$(this).empty();
-	 		// Defaults
-	   		var difficulty = 5;
-	   		var turns = 10;
+		 		// Starting values
+	   		var difficulty = 5,
+	   				turns = 10,
+	   				round = 1,
+	   				time = $.now(),
+	   				total = 0;
+	   				r = 0;
+	   				g = 0;
+	   				b = 0;
 	   		// If settings are present, assign them
 	   		if (settings) {
 	   			if (settings.difficulty) {
@@ -17,18 +23,13 @@ $(document).ready(function() {
 	   		}
 	   		// Add Title
 	   		this.append("<h1>Hexed</h1>");
-	   		// Add sliders
-	   		// this.append("<div id='spinners'>\
-	   		// 				<p class='desc'>Difficulty</p>\
-	   		// 				<input type='text' id='difficulty' value='0'/>\
-	   		// 				<p class='desc'>Turns</p>\
-	   		// 				<input type='text' id='turns' value='0'/>\
-	   		// 			</div>");
+	   		// Add Sliders
 	   		this.append("<p class='desc'>Difficulty</p>\
 	   					 <input type='text' id='difficulty' value='0'/>\
 	   					 <p class='desc'>Turns</p>\
 	   					 <input type='text' id='turns' value='0'/>");
 	   		$("#difficulty").spinner({
+	   			disabled: true,
 	   			min: 0,
 	   			max: 10,
 	   			change: function() { // limit greater/smaller values
@@ -45,6 +46,7 @@ $(document).ready(function() {
 				}
 	   		}).val(difficulty);
 	   		$("#turns").spinner({
+	   			disabled: true,
 	   			min: 0,
 	   			change: function() { // limit greater/smaller values
 			        var min = $(this).spinner('option', 'min');
@@ -57,16 +59,6 @@ $(document).ready(function() {
 			        }
 				}
 	   		}).val(turns);
-	   		// New game button
-	   		this.append("<div id='newGame'></div>");
-	   		$("#newGame").button({
-	   			label:"New Game"
-	   		}).on("click", function() {
-	   			$("#hexed").hexed({
-	   				difficulty: $("#difficulty").spinner("value"),
-	   				turns: $("#turns").spinner("value")
-	   			});
-	   		});
 	   		// Random Color box
 	   		var r = Math.floor(Math.random() * 255) + 1;
 	   		var g = Math.floor(Math.random() * 255) + 1;
@@ -76,8 +68,8 @@ $(document).ready(function() {
 	   					 <div id='red'></div>\
 	   					 <div id='green'></div>\
 	   					 <div id='blue'></div>");
-	   		$("#Lcolor").css("background-color", "rgb("+r+","+g+","+b+")")
-	   		// Player color box
+	   		$("#Lcolor").css("background-color", "rgb("+r+","+g+","+b+")");
+	   		// Player color box and rgb sliders
 	   		function refreshColor() {
 		    	var red = $("#red").slider("value"),
 		        	green = $("#green").slider("value"),
@@ -93,10 +85,68 @@ $(document).ready(function() {
 		      change: refreshColor
 		    });
 		    $("#red").slider("value",127);
-
-
-	 		// Return self for chaining
-	        return this;
+		    // New game button - hidden to start
+		    this.append("<div id='newGame'></div>");
+	   		$("#newGame").button({
+	   			label:"New Game?"
+	   		}).on("click", function() {
+	   			$("#hexed").hexed({
+	   				difficulty: $("#difficulty").spinner("value"),
+	   				turns: $("#turns").spinner("value")
+	   			});
+	   		}).hide();
+	   		// Next button - appears after Check It
+		   	this.append("<div id='next'></div>");
+	   		$("#next").button({
+	   			label:"Next"
+	   		}).on("click", function() {
+		    	// Increment round
+		    	$("#turns").val(turns-round);
+		    	round++;
+		    	// New Random Color
+		    	r = Math.floor(Math.random() * 255) + 1,
+	   			g = Math.floor(Math.random() * 255) + 1,
+	   			b = Math.floor(Math.random() * 255) + 1;
+	   			$("#Lcolor").css("background-color", "rgb("+r+","+g+","+b+")");
+	   			// Reset slider values
+	   			$("#red, #green, #blue").slider("value", 127);
+	   			$(this).hide();
+	   			$("#check").show();
+	   			// Reset time
+	   			time = $.now();
+	   		}).hide();
+	   		// Check It! button
+		   	this.append("<div id='check'></div>");
+	   		$("#check").button({
+	   			label:"Check It!"
+	   		}).on("click", function() {
+	   			// Calculate error and score
+	   			var delay = $.now()-time,
+	   					errorRed		= Math.abs(((r-$("#red").slider(	"option","value"))/255)*100),
+	   					errorGreen	= Math.abs(((g-$("#green").slider("option","value"))/255)*100),
+	   					errorBlue 	= Math.abs(((b-$("#blue").slider(	"option","value"))/255)*100),
+	   					error = (errorRed+errorGreen+errorBlue)/3,
+	   					score = ((15 - difficulty - error)/(15 - difficulty))*(15000-delay);
+	   			if (score<0) {score=0;} // Negative score
+	   			total += score; // Add round to total score
+	   			// Report the score
+	   			$("#score").empty().text("Score: " + score.toFixed(2));
+	   			$("#total").empty().text("Total: " + total.toFixed(2));
+	   			// Game over
+	   			if (round==turns) {
+	   				$(this).hide();
+	   				$("#newGame").show();
+	   				$("#difficulty, #turns").spinner("enable");
+	   				$("#turns").val(turns);
+	   			} else { // Next round/turn
+		   			$(this).hide();
+		   			$("#next").show();
+		   		}
+	   		});
+   			this.append("<div id='score'></div>\
+   								   <div id='total'></div>");
+	 			// Return self for chaining
+	      return this;
 	    };
 	}( jQuery ));
 	// Call initial game
